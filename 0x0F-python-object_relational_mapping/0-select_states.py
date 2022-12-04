0c79acae95e20de4b357cae4e024ae021408f4d7
DonAine <30406256+DonAine@users.noreply.github.com>
2022-12-04 00:20:31 -0800

    Documentation fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-
-
-import MySQLdb
-from sys import argv
-
 '''
 a script that lists all states
 from the database
 '''
+
+import MySQLdb
+from sys import argv
+
 if __name__ == "__main__":
     con = MySQLdb.connect(
         host="localhost", port=3306, user=argv[1],
