f0ded0c5b82638b9b04b05f50d3f5a1a211adadd
DonAine <30406256+DonAine@users.noreply.github.com>
2022-12-04 01:30:08 -0800

    Revert "Add"
    This reverts commit e99df824b9813511df808a78a721f8707ecb8d9f.

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -10,13 +10,6 @@ from model_state import Base, State
 
 
 class City(Base):
-    i """Represents a city for a MySQL database.
-        Attributes:
-                    id (Integer): The city's id.
-                    name (sqlalchemy.String): The city's name.
-                    state_id (sqlalchemy.Integer): The city's state id.
-      """
-
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
